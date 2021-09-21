bfae2f9eada5d3f82532911b55eada271423bdf8
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2021-09-21 14:50:13 +0530

    Done3

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter><App /></HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}><App /></BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
